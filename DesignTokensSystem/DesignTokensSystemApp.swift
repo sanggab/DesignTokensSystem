18a10f6900d0e27e007f379a63839e14eeb53354
심상갑 <kappa2410@gmail.com>
2026-01-09 09:13:52 +0900

    Initial Commit

---
+++ DesignTokensSystem/DesignTokensSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignTokensSystemApp.swift
+//  DesignTokensSystem
+//
+//  Created by Gab on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct DesignTokensSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DesignTokensSystemTests/DesignTokensSystemTests.swift
@@ -0,0 +1,16 @@
+//
+//  DesignTokensSystemTests.swift
+//  DesignTokensSystemTests
+//
+//  Created by Gab on 1/9/26.
+//
+
+import Testing
+
+struct DesignTokensSystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
